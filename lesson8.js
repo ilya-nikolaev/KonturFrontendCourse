082ef412b0d1fb3481e9e930a2eebead57b9965a
Ilya Nikolaev <65247719+ilya-nikolaev@users.noreply.github.com>
2024-01-12 03:07:27 +0300

    Completed lesson 8

---
+++ lesson8.js
@@ -0,0 +1,7 @@
+let age = prompt("Введите ваш возраст");  // String
+
+if (age > 65) {  // Compare String and Number, got Boolean
+    console.log("Вы пенсионер");
+} else {
+    console.log("Вы еще молодой");
+}

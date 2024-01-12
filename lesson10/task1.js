ece81f47473081f87a39f34b32848f0bf5109dd3
Ilya Nikolaev <65247719+ilya-nikolaev@users.noreply.github.com>
2024-01-12 03:06:55 +0300

    Completed lesson 10

---
+++ lesson10/task1.js
@@ -0,0 +1,10 @@
+((());  // noqa
+
+let x = undefinedX;
+
+null.x();
+
+let arr = new Array;
+console.log(arr[42]);
+
+decodeURIComponent("%%%");
---
+++ lesson10/task2.js
@@ -0,0 +1,9 @@
+throw new EvalError();
+// throw new InternalError();
+throw new RangeError();
+throw new ReferenceError();
+throw new SyntaxError();
+throw new TypeError();
+throw new URIError();
+
+throw new Error();
